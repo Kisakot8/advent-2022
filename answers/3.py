7b4830fa1ea00f582daa0a69d2089d4143388d13
Max <110265678+Kisakot8@users.noreply.github.com>
2022-12-03 13:20:10 +0100

    Create 3.py
    I now know that sets are a thing. They're pretty cool but had to have a fairly roundabout way to get the first element from them.
    
    I actually tried to have O(n) time complexity for this, which I think I succeeded in. That's why sometimes some lines are a little annoying to read (looking at you line 33) but I tried to avoid using nested for loops where possible

---
+++ answers/3.py
@@ -0,0 +1,39 @@
+import string
+
+def main():
+    with open('testing text.txt','r',encoding='utf-8') as f:
+        repeated = []
+        for line in f:
+            line = line.strip()
+            first_half = line[:len(line)//2]
+            second_half = line[len(line)//2:]
+            for letter in second_half:
+                if letter in first_half:
+                    repeated.append(letter)
+                    break
+    letters = [[x,string.ascii_letters.index(x)+1] for x in string.ascii_letters]
+    priorities = {k:v for (k,v) in letters}
+    priorities_sum = 0
+    for letter in repeated:
+        priorities_sum += priorities[letter]
+    
+    print(priorities_sum) # Part 1 solution
+
+    with open('testing text.txt','r',encoding='utf-8') as f:
+        groups = []
+        temp = []
+        for i, line in enumerate(f, start=1):
+            line = line.strip()
+            temp.append(line)
+            if i%3 == 0:
+                groups.append(temp)
+                temp = []
+    badges = []
+    for group in groups:
+        badges.append(list(set(group[0]) & set(group[1]) & set(group[2]))[0])
+
+    badges_priorities = [priorities[x] for x in badges]
+    print(sum(badges_priorities)) # Part 2 solution
+
+if __name__ == '__main__':
+    main()
